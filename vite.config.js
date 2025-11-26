62804652962e8a1ac5bbcf1f2f8ddc550e873900
Aires23Matos <aires2matos18@gmail.com>
2025-11-26 16:31:08 +0100

    new port

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+//const url= "http://localhost:3000"
+const url = "https://customer-management-api-with-typescript.onrender.com";
 export default defineConfig({
-  plugins: [
-    react(),
-     tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     "/api": {
-      target: "http://localhost:3000"
-    }
+      target: url,
+    },
   },
-   build: {
-    outDir: 'dist',
-    sourcemap: true, 
-    minify: 'esbuild', 
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    minify: "esbuild",
   },
-})
+});
